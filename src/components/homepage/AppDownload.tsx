bd692940cff2c811b30ce4263d07bbe3f3cd6103
Yauhen Davidovich <davidovich336@gmail.com>
2021-12-22 11:15:49 +0100

    feat: add app download block adaptive

--- src/components/homepage/AppDownload.tsx
+++ src/components/homepage/AppDownload.tsx
@@ -3,6 +3,7 @@ import BackgroundColorWrapper from "../BackgroundColorWrapper";
 import styled from "styled-components";
 import BlockTitle from "../BlockTitle";
 import Iphone from "../../assets/images/Iphone.png"
+import Astronaut from "../../assets/images/Astronaut.png"
 import PlayMarket from "../../assets/images/playmarket.png"
 import Appstore from "../../assets/images/appstore.png"
 import {StyledPlaceVacancy} from "./PlaceVacancy";
@@ -16,14 +17,41 @@ const BackgroundColorWrapperExtended = styled(BackgroundColorWrapper)`
 
 const StyledAppDownloadWrapper = styled(StyledPlaceVacancy)`
   padding-top: -120px;
+
+  .mobileImage {
+    display: none;
+  }
+
   .buttonsBlock {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     min-height: 135px;
     height: 100%;
+    margin-top: 10px;
+    margin-bottom: 5px;
+
   }
-  
+
+  @media ${props => props.theme.media.phone} {
+    flex-direction: column;
+    max-width: 335px;
+    .mobileImage {
+      display: block;
+      margin-top: 5px;
+      margin-bottom: 5px;
+    }
+
+    .desktopImage {
+      display: none;
+    }
+
+    .downloadText {
+      text-align: left;
+      margin-top: 15px;
+      margin-bottom: 5px;
+    }
+  }
 `
 
 
@@ -31,9 +59,11 @@ const AppDownload = () => {
     return (
         <BackgroundColorWrapperExtended backgroundColor={"#FAF5FF"}>
             <StyledAppDownloadWrapper>
-                <div><img src={Iphone}
-                          alt="Iphone"/></div>
-                <div className={'placeVacancyText'}>
+                <div className={"desktopImage"}><img src={Iphone}
+                                                     alt="Iphone"/></div>
+                <div className={"mobileImage"}><img src={Astronaut}
+                                                    alt="Astronaut"/></div>
+                <div className={'downloadText'}>
                     <BlockTitle title={'Скачивайте приложение'}/>
                     <div><h4>Работодатели смогут найти вас и предложить отличную работу.</h4></div>
                 </div>
@@ -43,8 +73,6 @@ const AppDownload = () => {
                 </div>
 
             </StyledAppDownloadWrapper>
-
-
         </BackgroundColorWrapperExtended>
     );
 };
